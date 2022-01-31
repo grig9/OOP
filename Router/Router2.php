5c71115d83530f132db30079265782ffae5cac21
grig9 <grigorevalex91@gmail.com>
2022-01-31 18:20:12 +0300

    1

--- Router/Router2.php
+++ Router/Router2.php
@@ -1,5 +1,12 @@
 <?php
-// методы это инструменты для маршрутизатора
+// цель - создать простой маршрутизатор
+// разбираюсь что такое маршрутизатор, как он работает.
+// интерфейс, как я хочу им пользоваться
+  // $route = new Route($array_routes);
+  // $route->run();
+// создаем конструктор принимающий ассоциативный массив
+// создаем метод run()
+
 class Route2 {
   protected $routes;
 
@@ -9,20 +16,12 @@ class Route2 {
 
   public function run() {
     $route = $_SERVER['REQUEST_URI'];
-    var_dump($route);
     if(array_key_exists($route, $this->routes)) {
-      // var_dump(include( $this->routes[$route] ));
-      echo "Маршрут найден";
+      include( $this->routes[$route] );
       return true;
     } else {
-        echo "Маршрут не найден";
+        echo "Маршрут не найден 404";
         return false;
-      }
     }
-
-
   }
-
-  
-  
-;?>
+}
